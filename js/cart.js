6201462e55261e56b47738dc7435a38937b47d57
In5lux <85581533+In5lux@users.noreply.github.com>
2021-11-26 14:12:25 +0300

    cart modal

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+const cart = function () {
+  const cartBtn = document.querySelector(".button-cart");
+  const cart = document.querySelector("#modal-cart");
+  const closeBtn = document.querySelector(".modal-close");
+  cartBtn.addEventListener("click", function () {
+    cart.style.display = "flex";
+    closeBtn.addEventListener("click", () => (cart.style.display = "none"));
+  });
+};
+
+cart();
