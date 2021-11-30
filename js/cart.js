24a4c9067b84066a4ff6f1e102fa678e76ec7771
In5lux <nikos_almeryda@mail.ru>
2021-11-30 19:54:48 +0300

    Modal close & smooth scroll

--- js/cart.js
+++ js/cart.js
@@ -2,9 +2,26 @@ const cart = function () {
   const cartBtn = document.querySelector(".button-cart");
   const cart = document.querySelector("#modal-cart");
   const closeBtn = document.querySelector(".modal-close");
+
+  const goodsContainer = document.querySelector('.long-goods-list');
+
+  const cartTable = document.querySelector('.cart-table__goods');
+
+  cart.addEventListener('click',(event)=>{
+    if(!event.target.closest('.modal')){
+      cart.style.display = "none"
+    }
+  });
+
+
+
   cartBtn.addEventListener("click", function () {
     cart.style.display = "flex";
     closeBtn.addEventListener("click", () => (cart.style.display = "none"));
+  });
+
+  window.addEventListener('keydown',(e)=>{
+    if(e.key==='Escape'){cart.style.display = "none"}
   });
 };
 
